b01d212d80dce9e2240cbc792e924ef716428aa2
Nikkru <56563372+Nikkru@users.noreply.github.com>
2020-05-22 22:13:43 +0300

    finished

--- HomeWorkSecondRGB/ViewController.swift
+++ HomeWorkSecondRGB/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var rgbView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // rgbView
         rgbView.layer.cornerRadius = 10
-        rgbView.backgroundColor = UIColor.init(red: 0.5, green: 0.2, blue: 0.1, alpha: 1)
+        rgbView.backgroundColor = UIColor.black
+        
+        // slider
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
-
-
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        redLabel.text = NSString(format: "%.2f", redSlider.value) as String
+        blueLabel.text = NSString(format: "%.2f", blueSlider.value) as String
+        greenLabel.text = NSString(format: "%.2f", greenSlider.value) as String
+        
+        rgbView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
