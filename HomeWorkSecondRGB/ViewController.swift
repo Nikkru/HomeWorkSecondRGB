c63644435f43f68fc7f9aa0137e83e2ef7d517dd
Nikkru <56563372+Nikkru@users.noreply.github.com>
2020-05-22 18:11:35 +0300

    Initial Commit

---
+++ HomeWorkSecondRGB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkSecondRGB
+//
+//  Created by Nikolai Krusser on 22.05.2020.
+//  Copyright © 2020 Nikolai Krusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
